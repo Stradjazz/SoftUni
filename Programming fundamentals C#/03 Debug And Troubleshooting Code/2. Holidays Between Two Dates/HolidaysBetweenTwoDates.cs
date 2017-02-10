e3b050a57385069e635727e59d974e2a2de49f7a
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-02-10 16:05:56 +0100

    update

--- Programming fundamentals C#/03 Debug And Troubleshooting Code/2. Holidays Between Two Dates/HolidaysBetweenTwoDates.cs
+++ Programming fundamentals C#/03 Debug And Troubleshooting Code/2. Holidays Between Two Dates/HolidaysBetweenTwoDates.cs
@@ -16,7 +16,30 @@ class HolidaysBetweenTwoDates
         var holidaysCount = 0;
         for (var date = startDate; date <= endDate; date = date.AddDays(1))
             if (date.DayOfWeek == DayOfWeek.Saturday ||
+                date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                holidaysCount++;
+            }
+        Console.WriteLine(holidaysCount);
+    }
+    /*  Broken code:
+        using System;
+using System.Globalization;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var endDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var holidaysCount = 0;
+        for (var date = startDate; date <= endDate; date.AddDays(1))
+            if (date.DayOfWeek == DayOfWeek.Saturday &&
                 date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
         Console.WriteLine(holidaysCount);
     }
+}
+*/
 }
