30eda361abcb1668645cb4af77d791ae21541f76
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-02-13 17:01:31 +0100

    update and renaming

---
+++ Programming fundamentals C#/04. Simple Array Processing Lab/1. Sum Array Elements/SumArrayElements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Sum_Array_Elements
+{
+    //Write a program which reads an array of integers, calculates its sum and prints it.
+    //The input consists of a number n(the number of elements) + n integers, each as a separate line.
+
+    class SumArrayElements
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            long sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i];
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programming fundamentals C#/04. Simple Array Processing Lab/4. Rotate Array of Strings/RotateArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Rotate_Array_of_Strings
+{
+    //Write a program to read an array of strings, rotate it to the right and print its rotated elements.
+    class RotateArray
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split().ToArray();
+            string[] rotatedArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                rotatedArray[i + 1] = array[i];
+            }
+
+            var lastElement = array[rotatedArray.Length - 1];
+            rotatedArray[0] = lastElement;
+
+            Console.WriteLine(string.Join(" ", rotatedArray));
+        }
+    }
+}
