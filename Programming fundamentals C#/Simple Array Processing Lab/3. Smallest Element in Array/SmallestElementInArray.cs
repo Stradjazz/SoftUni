7ef55e96b05848eebef8bba4380a746852da46e1
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-02-13 16:57:04 +0100

    upload of new homeworks

---
+++ Programming fundamentals C#/Simple Array Processing Lab/3. Smallest Element in Array/SmallestElementInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Smallest_Element_in_Array
+{
+    //Write a program to read an array of integers, find the smallest element and print it.
+
+    class SmallestElementInArray
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var minNumber = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minNumber)
+                {
+                    minNumber = numbers[i];
+                }
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
---
+++ Programming fundamentals C#/Simple Array Processing Lab/5. Count of Odd Numbers in Array/CountOddNums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Count_of_Odd_Numbers_in_Array
+{
+    //Write a program to read an array of integers and find how many odd elements it holds.
+    class CountOddNums
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
