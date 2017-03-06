27fedf77974b0c42d677bf82480be68d85fcd443
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-03-06 01:11:16 +0100

    Upload of new exercises

---
+++ Programming fundamentals C#/05. Array and List Algorithms 2/01. Rabbit Hole/RabbitHole.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Rabbit_Hole
+{
+    /*  Rabbit hole is a term for a portal for time travel. 
+You are trying to go back in the past to prevent Kennedy’s assassination. But first you should find the rabbit hole. 
+Your energy is limited and if you exhaust it, you will be tired and can’t continue the mission.
+You will be given an array of strings representing different obstacles you should overcome. 
+•	“Left|[integer value]”-you should jump to the left with [integer value] positions and decrease your energy with [integer value];
+•	“Right|[integer value]”-you should jump to the right with [integer value] positions and decrease your energy with [integer value];
+•	“Bomb|[integer value]”-the bomb explodes and this element of the array should be removed, your energy should be decreased be the [integer value], 
+    you should start from the beginning (index 0);
+•	“RabbitHole” – you have found the rabbit hole, the program should stop here, print on the console – “You have 5 years to save Kennedy!”
+ 
+Your mission begins at the first element of the array. The rabbit hole will be only one.
+The program ends when you find the rabbit hole or when your energy is gone. And if it is gone due to bomb explosion you should print 
+“You are dead due to bomb explosion!” on the console or if it is due jumps print “You are tired. You can't continue the mission.”.
+After every move someone puts a bomb at the end of the array with [integer value] of your current energy (the last element is removed and the bomb is 
+placed there, but when the last element is the “RabbitHole”, it can’t be removed and the bomb is placed after it).
+*/
+    class RabbitHole
+    {
+        static void Main(string[] args)
+        {
+            List<string> command = Console.ReadLine().Split(' ').ToList();
+            int energy = int.Parse(Console.ReadLine());
+
+            int currentIndex = 0;
+            bool deadByBomb = false;
+
+            while (energy > 0)
+            {
+                deadByBomb = false;
+                string[] currentCommand = command[currentIndex].Split('|');
+                string action = currentCommand[0];
+
+                if (action == "RabbitHole")
+                {
+                    Console.WriteLine("You have 5 years to save Kennedy!");
+                    break;
+                }
+
+                int value = int.Parse(currentCommand[1]);
+
+                switch (action)
+                {
+
+                    case "Left":
+                        currentIndex = Math.Abs(currentIndex - value) % command.Count;
+                        energy -= value;
+                        break;
+
+                    case "Right":
+                        currentIndex = (currentIndex + value) % command.Count;
+                        energy -= value;
+                        break;
+
+                    case "Bomb":
+                        command.RemoveAt(currentIndex);
+                        currentIndex = 0;
+                        energy -= value;
+                        deadByBomb = true;
+                        break;
+                }
+
+                if (command[command.Count - 1] != "RabbitHole")
+                {
+                    command.RemoveAt(command.Count - 1);
+                }
+                command.Add("Bomb|" + energy);
+
+
+                if (energy <= 0 && deadByBomb)
+                {
+                    Console.WriteLine("You are dead due to bomb explosion!");
+                }
+                else if (energy <= 0 && !deadByBomb)
+                {
+                    Console.WriteLine("You are tired. You can't continue the mission.");
+                }
+            }
+
+        }
+    }
+}
---
+++ Programming fundamentals C#/05. Array and List Algorithms 2/02. Japanese Roulette/JapaneseRoulette.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Japanese_Roulette
+{
+    /*Russian roulette is a game of chance where a single player places a single bullet in a revolver, spins the cylinder, 
+     points the muzzle against their head and pulls the trigger. Japanese people however are not that brave (crazy) so they play with toy guns.
+    Every player can spin the cylinder with different strength. The numeric representation of the strength is with how many places they can move 
+    the bullet while spinning. Note that the cylinder has only 6 places but the strength could be much more than 6 due to several 
+    rotations of the cylinder.  
+    You are given an array of integers which represent the cylinder of the revolver where 0 means empty and 1 represents the bullet. 
+    There is exactly one bullet in the cylinder.
+    You will receive another array, this time of strings where every index is a different player and every value is the strength of the player and 
+    the direction to which the player rotates the cylinder.
+    The elements in the array will be separated by spaces. The power and the direction will be separated by a comma (”,”) e.g.: “111,Left”. 
+    The muzzle IS at index 2 of the cylinder. If the element at that position is 1, the current player loses the game.
+ 
+    Note! After a player pulls the trigger the cylinder spins with one position to the right. 
+    The next player starts spinning with the new state of the cylinder.
+    Assume that the order of the players is according to their places in the array. The player with index 0 shots first, index 1-second and so on…
+    Your task is to go through all players, and see if anyone will shoot himself with the toy gun. */
+    class JapaneseRoulette
+    {
+        static void Main(string[] args)
+        {
+            var cylinder = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var players = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            var deadMan = false;
+            var deadPlayer = 0;
+
+            foreach (var player in players)
+            {
+                var spinInfo = player
+                    .Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                    .Select(a => a.Trim())
+                    .ToArray();
+
+                var spinStrength = int.Parse(spinInfo[0]) % cylinder.Count;
+                var spinDirection = spinInfo[1];
+
+                if (spinDirection == "Left")
+                {
+                    cylinder = SpinLeft(cylinder, spinStrength);
+
+                }
+                else if (spinDirection == "Right")
+                {
+                    cylinder = SpinRight(cylinder, spinStrength);
+                }
+
+                if (cylinder[2] == 1)
+                {
+                    deadMan = true;
+                    deadPlayer = players.IndexOf(player);
+                    break;
+                }
+                cylinder = SpinRight(cylinder, 1); //Trigger
+            }
+
+            if (deadMan)
+            {
+                Console.WriteLine($"Game over! Player {deadPlayer} is dead.");
+            }
+            else
+            {
+                Console.WriteLine("Everybody got lucky!");
+            }
+        }
+
+        private static List<int> SpinLeft(List<int> cylinder, int spinStrength)
+        {
+            for (int i = 0; i < spinStrength; i++)
+            {
+                cylinder = cylinder
+                    .Skip(1)
+                    .Concat(cylinder.Take(1))
+                    .ToList();
+            }
+            return cylinder;
+        }
+
+        private static List<int> SpinRight(List<int> cylinder, int spinStrength)
+        {
+            for (int i = 0; i < spinStrength; i++)
+            {
+                cylinder = cylinder
+                    .Skip(cylinder.Count - 1)
+                    .Concat(cylinder.Take(cylinder.Count - 1))
+                    .ToList();
+            }
+            return cylinder;
+        }
+
+    }
+}
