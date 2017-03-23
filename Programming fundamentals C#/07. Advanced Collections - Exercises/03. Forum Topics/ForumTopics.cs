2fc5781016afa764a4928e8054091ae5e2c16b53
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-03-23 12:25:39 +0100

    Update of homeworks

--- Programming fundamentals C#/07. Advanced Collections - Exercises/03. Forum Topics/ForumTopics.cs
+++ Programming fundamentals C#/07. Advanced Collections - Exercises/03. Forum Topics/ForumTopics.cs
@@ -20,7 +20,7 @@ namespace _03.Forum_Topics
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> topics = new Dictionary<string, List<string>>();
+            Dictionary<string, HashSet<string>> topics = new Dictionary<string, HashSet<string>>();
             string inputline = Console.ReadLine();
 
             while (inputline != "filter" )
@@ -30,7 +30,7 @@ namespace _03.Forum_Topics
                 
                 if (!topics.ContainsKey(currentKey))
                 {
-                    topics[currentKey] = new List<string>();
+                    topics[currentKey] = new HashSet<string>();
                 }
                 for (int i = 1; i < inputData.Length; i++)
                 {
@@ -39,33 +39,31 @@ namespace _03.Forum_Topics
                 inputline = Console.ReadLine();
             }
             var tagSequence = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-            var keysToInclude = new List<string>();
-
-            foreach (var topic in topics)
-            {
-                var currentKey = topic.Key;
-                var currentTagList = topic.Value;
-                var tagCounter = 0;
-                foreach (var tag in tagSequence)
-                {
-                    if (currentTagList.Contains(tag))
-                    {
-                        tagCounter++;
-                    }
-                    if (tagSequence.Count == tagCounter)
-                    {
-                        keysToInclude.Add(currentKey);
-                    }
-                }
-                var keysToRemove = topics.Keys.Except(keysToInclude).ToList();
-                foreach (var keyEntry in keysToRemove)
-                {
-                    topics.Remove(keyEntry);
-                }
-            }
+            
             foreach (var entry in topics)
             {
-                Console.WriteLine($"{entry.Key} | #{string.Join(", #", entry.Value)}");
+                bool containsTag = false;
+                for (int i = 0; i < tagSequence.Count; i++)
+                {
+                    if (entry.Value.Contains(tagSequence[i]))
+                    {
+                        containsTag = true;
+                    }
+                    else
+                    {
+                        containsTag = false;
+                        break;
+                    }
+                }
+
+                var topic = entry.Key;
+                var tags = entry.Value;
+
+                if (containsTag)
+                {
+                    Console.WriteLine($"{topic} | #{string.Join(", #", tags)}");
+                }
+                
             }           
         }
     }
--- Programming fundamentals C#/07. Advanced Collections - Exercises/04. Social Media Posts/SocialMediaPosts.cs
+++ Programming fundamentals C#/07. Advanced Collections - Exercises/04. Social Media Posts/SocialMediaPosts.cs
@@ -6,10 +6,112 @@ using System.Threading.Tasks;
 
 namespace _04.Social_Media_Posts
 {
+    /*  You have been tasked to create a Console Social Media Database.
+You will receive several input lines in one of the following formats:
+•	post {postName}
+•	like {postName}
+•	dislike {postName}
+•	comment {postName} {commentatorName} {content}
+If you receive the post command, you must create a post with the given name. If you receive the like command you must add a like to the given post.
+If you receive the dislike command you must add a dislike to the given post.
+If you receive the comment command, you must add a comment to the given post. The comment’s writer must be the given commentator name, 
+and the content of the comment must be the given content. By default the posts have 0 likes, 0 dislikes and 0 comments, when created.
+When you receive the command “drop the media” you must end the input sequence, and you must print every post with its likes, disliked and 
+comments in the following format:
+Post: {postName} | Likes: {likes} | Dislikes {dislikes}
+Comments:
+*  {commentator1}: {comment1}
+*  {commentator2}: {comment2}
+If a certain post does NOT have ANY comments, you should just print “None”.
+The comments have a prefix of a single asterisk (‘*’) and 2 spaces. There is NO space between the commentator’s name and the colon. */
     class SocialMediaPosts
     {
         static void Main(string[] args)
         {
+            var socialMedia = new Dictionary<string, Dictionary<string, List<string>>>();
+            var input = Console.ReadLine();
+
+            while (input != "drop the media")
+            {
+                var tokens = input.Split(' ').ToArray();
+                var command = tokens[0];
+                var postName = tokens[1];
+
+                if (!socialMedia.ContainsKey(postName))
+                {
+                    socialMedia[postName] = new Dictionary<string, List<string>>();
+                }
+                if (command == "like")
+                {
+                    if (!socialMedia[postName].ContainsKey("Like"))
+                    {
+                        socialMedia[postName]["Like"] = new List<string>();
+                    }
+
+                    socialMedia[postName]["Like"].Add(command);
+                }
+                else if (command == "dislike")
+                {
+                    if (!socialMedia[postName].ContainsKey("Dislike"))
+                    {
+                        socialMedia[postName]["Dislike"] = new List<string>();
+                    }
+
+                    socialMedia[postName]["Dislike"].Add(command);
+                }
+                else if (command == "comment")
+                {
+                    var writer = tokens[2];
+                    int length = command.Length + postName.Length + writer.Length + 3;
+                    var comment = input.Substring(length);
+                    if (!socialMedia[postName].ContainsKey(writer))
+                    {
+                        socialMedia[postName][writer] = new List<string>();
+                    }
+
+                    socialMedia[postName][writer].Add(comment);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var post in socialMedia)
+            {
+                var likes = 0;
+                var dislikes = 0;
+                var writers = post.Value;
+                foreach (var item in post.Value)
+                {
+                    if (item.Key == "Like")
+                    {
+                        likes = item.Value.Count;
+                    }
+                    else if (item.Key == "Dislike")
+                    {
+                        dislikes = item.Value.Count;
+                    }
+                }
+                Console.WriteLine($"Post: {post.Key} | Likes: {likes} | Dislikes: {dislikes}");
+                Console.WriteLine("Comments:");
+
+                bool noComment = true;
+                foreach (var comment in writers)
+                {
+                    if (comment.Key != "Like" && comment.Key != "Dislike")
+                    {
+                        noComment = false;
+                        foreach (var com in comment.Value)
+                        {
+                            Console.WriteLine($"*  {comment.Key}: {com}");
+                        }
+                    }
+                }
+
+                if (noComment)
+                {
+                    Console.WriteLine("None");
+                }
+            }
         }
     }
 }
