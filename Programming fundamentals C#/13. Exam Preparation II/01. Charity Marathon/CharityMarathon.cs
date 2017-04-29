1d082c3c153ed8e4e84410913a2e1ae62cc3ca7f
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-04-30 01:07:10 +0200

    Upload homeworks

---
+++ Programming fundamentals C#/13. Exam Preparation II/01. Charity Marathon/CharityMarathon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Charity_Marathon
+{
+    class CharityMarathon
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine()); 
+            int lapsPerRunner = int.Parse(Console.ReadLine()); 
+
+            long trackLength = long.Parse(Console.ReadLine()); 
+            int trackCapacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKm = decimal.Parse(Console.ReadLine());
+
+            trackCapacity = days * trackCapacity;
+            runners = Math.Min(trackCapacity, runners);
+            long totalMeters = runners * trackLength * lapsPerRunner;
+            long totalKm = totalMeters / 1000;
+            var raisedMoney = totalKm * moneyPerKm;
+
+            Console.WriteLine($"Money raised: {raisedMoney:F2}");
+
+        }
+    }
+}
---
+++ Programming fundamentals C#/13. Exam Preparation II/03. Nether Realms/Demon.cs
@@ -0,0 +1,9 @@
+﻿namespace _03.Nether_Realms
+{
+    public class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+    }
+}
