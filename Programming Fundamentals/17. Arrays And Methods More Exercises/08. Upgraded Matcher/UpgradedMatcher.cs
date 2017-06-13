2dc723bb17ef275ea0bdfeb3bcf6ea05f9523ba6
IvayloDechkoff <Stradjazz1973@gmail.com>
2017-06-13 23:47:30 +0200

    Update of homeworks

--- Programming Fundamentals/17. Arrays And Methods More Exercises/08. Upgraded Matcher/UpgradedMatcher.cs
+++ Programming Fundamentals/17. Arrays And Methods More Exercises/08. Upgraded Matcher/UpgradedMatcher.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace _08.Upgraded_Matcher
 {
+    /*For this task, you can use your solution from Inventory Matcher. You will again receive 3 arrays – one with strings, one with longs and one with decimals. 
+    Again, the price and quantity correspond to a name, which is located on same index as the name.
+    This time only the arrays containing the names and the array containing the prices will have the same length. If in the quantities array there is no index, which corresponds 
+    to the name, you should assume the quantity is 0.
+    On top of that the products, which you receive after the arrays will contain not only a string for the name, but also a long, which is the quantity that must be ordered. 
+    If you have enough quantity, calculate the total price by multiplying the ordered quantity times the price and print it in the following format:
+    {quantity ordered} x {product name} costs {total price of the order}
+    Format the price to the 2nd decimal place. Do not forget to decrease the quantity of the product.
+    If you do not have enough quantities print: We do not have enough {product name} */
     class UpgradedMatcher
     {
         static void Main(string[] args)
         {
+            string[] products = Console.ReadLine().Split(' ').ToArray();
+            long[] quantities = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+            decimal[] prices = Console.ReadLine().Split(' ').Select(decimal.Parse).ToArray();
+            int index = 0;
+            
+
+            string input = Console.ReadLine();
+
+            while (input != "done")
+            {
+                string[] inputParts = input.Split(' ').ToArray();
+                string currentProduct = inputParts[0];
+                long orderQuantity = long.Parse(inputParts[1]);
+                
+                index = Array.IndexOf(products, currentProduct);
+                decimal totalPrice = orderQuantity * prices[index];
+
+                if (index > quantities.Length - 1 || orderQuantity > quantities[index])
+                {
+                    Console.WriteLine($"We do not have enough {products[index]}");
+                }
+                else
+                {
+                    Console.WriteLine($"{products[index]} x {orderQuantity} costs {totalPrice:F2}");
+                    quantities[index] -= orderQuantity;
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
---
+++ Programming Fundamentals/17. Arrays And Methods More Exercises/09. Jump Around/JumpAround.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Jump_Around
+{
+    /*You will receive an integer array from the console. You start from the beginning of the array and try to move right by a step, equal to the value at position 0. If that 
+    is possible you should collect the value from the index on which you landed, and try to move to the right by its value, if that is not possible – try to move to the left. 
+    If that is also not possible stop the program and print the sum of the collected values. */
+    class JumpAround
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var index = 0;
+            var sum = numbers[index];
+            var length = numbers.Length - 1;
+
+            while (true)
+            {
+                if (index + numbers[index] <= length)
+                {
+                    index += numbers[index];
+                    sum += numbers[index];
+                }
+                else if (index + numbers[index] >= 0)
+                {
+                    if (length - 1 - numbers[index] >= 0)
+                    {
+                        index -= numbers[index];
+                        sum += numbers[index];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
