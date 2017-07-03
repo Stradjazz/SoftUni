e25bdc654642865f7266a0f9f53bca9380135cee
Stradjazz <ivodechkoff@telefonica.net>
2017-07-03 17:14:57 +0200

    Upload exam preparation

---
+++ Programming Fundamentals/33. Exam Preparation/01. Sino The Walker/SinoTheWalker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace _01.Sino_The_Walker
+{
+    /*Sino is one of those people that lives in SoftUni. He leaves every now and then, but when he leaves he always takes a different route, so he needs to know how much time it 
+    will take for him to go home. Your job is to help him with the calculations.
+    You will receive the time that Sino leaves SoftUni, the steps taken and time for each step in seconds. 
+    You need to print the exact time that he will arrive at home in the format specified. */
+    class SinoTheWalker
+    {
+        static void Main(string[] args)
+        {
+            var timeFormat = @"hh\:mm\:ss";
+            TimeSpan leavingTime = TimeSpan.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+            var secondsPerDay = 60 * 60 * 24;
+            var steps = int.Parse(Console.ReadLine()) % secondsPerDay;
+            var secondsPerStep = int.Parse(Console.ReadLine()) % secondsPerDay;
+
+            var totalSeconds = steps * secondsPerStep;
+            var arrivalTime = leavingTime.Add(new TimeSpan(0, 0, (int)totalSeconds));
+
+            Console.WriteLine($"Time Arrival: {arrivalTime.ToString(timeFormat)}");
+        }
+    }
+}
---
+++ Programming Fundamentals/33. Exam Preparation/02. SoftUni Karaoke/SoftuniKaraoke.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SoftUni_Karaoke
+{
+    /*SoftUni cultivates talent whether it's coding talent or something else and in this case, something else is singing. Since you love music you want to take part in the event but as a programmer you simply lack 
+    the "something else" so your job is to make the software to track participants' awards.
+    •	On the first line, you will receive a list with all participants that applied for performance. 
+    •	On the second line, you will receive the list with all available songs. 
+    •	On the next lines, until the "dawn" command, you will get the names of people, the song that they are performing on stage and the award they get from the audience.
+    However, not every time the plan goes according to schedule. In other words, everyone (listed or not) can go on stage and perform a song that is not even available and he still gets awards from the audience. However, 
+    you should record only the awards for listed participants that perform songs available in the list. In case someone is not listed or sings a song that is not available you should not record neither the participant, nor 
+    his award.  
+    When the "dawn" comes, you need to print all participants, the count of the unique awards they received and all unique awards. Participants should be sorted by number of awards in descending order and then by 
+    participant name alphabetically. Awards should be sorted in alphabetical order.*/
+    class SoftuniKaraoke
+    {
+        static void Main(string[] args)
+        {
+            var participants = Console.ReadLine().Split(',').Select(x => x.Trim()).ToArray();
+            var songList = Console.ReadLine().Split(',').Select(x => x.Trim()).ToArray();
+            var participantsAwards = new Dictionary<string, SortedSet<string>>();
+
+            var input = Console.ReadLine();
+
+            while (input != "dawn")
+            {
+                var tokens = input.Split(',').Select(x => x.Trim()).ToArray();
+                var singer = tokens[0];
+                var song = tokens[1];
+                var award = tokens[2];
+
+                if (participants.Contains(singer) && songList.Contains(song))
+                {
+                    if (!participantsAwards.ContainsKey(singer))
+                    {
+                        participantsAwards[singer] = new SortedSet<string>();
+                    }
+                    participantsAwards[singer].Add(award);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (!participantsAwards.Any())
+            {
+                Console.WriteLine("No awards");
+            }
+            var sortedAwards = participantsAwards
+                    .OrderByDescending(x => x.Value.Count)
+                    .ThenBy(x => x.Key)
+                    .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var participant in sortedAwards)
+            {
+                Console.WriteLine($"{participant.Key}: {participant.Value.Count} awards");
+                var currentAwards = participant.Value;
+                foreach (var award in currentAwards)
+                {
+                    Console.WriteLine($"--{award}");
+                }
+            }
+        }
+    }
+}
